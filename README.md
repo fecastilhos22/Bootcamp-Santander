b268db005049eb3366d071c738734d62330317b1
Bowie <fecastilhos@gmail.com>
2025-06-11 17:44:26 -0300

    aulas 1 a 4

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Bootcamp Santander
 Bootcamp Santander 2025 - Back-End com Python
+Em parceria com a DIO
---
+++ print_input.py
@@ -0,0 +1,7 @@
+nome = input ("Informe seu nome: ")
+idade = input ("Informe sua idade: ")
+
+print(nome, idade)
+print("teste", end="")
+print(nome, idade, end="...\n") #n da a quebra de linha
+print (nome,idade, sep="#")
