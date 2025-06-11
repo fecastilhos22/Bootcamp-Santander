a0ef33121eeec0a9c9500f8989c0684d96569bf6
Bowie <fecastilhos@gmail.com>
2025-06-11 17:40:44 -0300

    Aula4

---
+++ variaveis_constantes.py
@@ -0,0 +1,22 @@
+#variaveis
+nome = "Guilherme"
+idade = 26
+
+nome = "Giovana"
+
+print (nome,idade)
+
+#boas_praticas
+limite_saque_diario = 1000
+limiteSaqueDiario = 1000
+
+#pessimas_praticas
+a = 10
+g = 20
+lim_saq_di = 1000
+
+
+#constantes
+BRAZILIAN_STATES = {'SP', 'RJ'}
+
+print(BRAZILIAN_STATES)
