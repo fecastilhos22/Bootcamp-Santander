77605ca8e90da17eb4618d221988998b068a77e7
Bowie <fecastilhos@gmail.com>
2025-06-23 15:25:13 -0300

    Aulas de tipos de operadores

---
+++ operadores_associacao.py
@@ -0,0 +1,7 @@
+frutas = ["limão", "uva"]
+curso = "Curso de Pythom"
+##sensível a letras maiusculas e minisculas(case-sentive)
+
+print ("laranja" not in frutas)
+print("limao" in frutas)
+print("Python" in curso)
---
+++ operadores_identidade.py
@@ -0,0 +1,7 @@
+#são operadores utilizados para comparar se os dois objetos testados ocupam a mesma posição na memória
+
+saldo = 1000
+limite = 500 
+
+print(saldo is limite)
+print(saldo is not limite)
